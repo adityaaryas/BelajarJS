647b44be56041111c4c58ed2b28757b014d5b088
adityaaryas <144906564+adityaaryas@users.noreply.github.com>
2023-10-12 17:09:00 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+// console.log("Hai doy")
+// alert("ada notifikasi")
+// prompt("Pinjam Seratus")
+
+// var Promnet = "Shilda is mbrot"
+// console.log(Promnet)
+// var Promnet = "Shilda not mbrot"
+// console.log(Promnet)
+
+// console.log(Promnet)
+// var.Promnet = "Shadow Kill"
+// console.log(Promnet)
+
+// let Promnet = "Coding Isfa Lana"
+// console.log(Promnet)
+// let Promnet = "Coding Isfa Lana"
+// console.log(Promnet)
+
+// const Promnet = "Coding is isun"
+// console.log(Promnet)
+// const Promnet = "Coding not isun"
+// console.log(Promnet)
+
+// let totalPoin = prompt("Massukan Poin Anda")
+//     if(totalPoin >= 100){
+//         document.write("Keren");
+//     }else{
+//         document.write("Pinjam dulu seratus");
+//     }
+
+// let x = 6;
+// let y = 3;
+
+    // if(x < 10 && y > 1){
+    //     document.write("True");
+    // }else{
+    //     doucument.write("False");
+    // }
+    
+
+    // console.log(x < 10 && y > 1);
+    // console.log(x < 10 && y < 1);
+    // console.log (x == 5 || y==5);
+    // console.log(x==6 || y==5);
+    // console.log(! (x==y));
+
+    // document.write(x < 10 && y > 1);
+    // document.write("<Br>");
+    // document.write(x < 10 && y < 1);
+    // document.write("<Br>");
+    // document.write(x == 5 || y==5);
+    // document.write("<Br>");
+    // document.write(x==6 || y==5);
+    // document.write("<Br>");
+    // document.write(! (x==y));
+
+    // let p = document.querySelector("p")
+    // let button = document.querySelector("button")
+    // let input = document.querySelector("input")
+    // button.addEventListener('clik',function(){
+    //     let isi = input.value
+    //     console.log(isi)
+    //     p.innerHTML = isi
+    // })
