fb45115f51cb1e34ffd96466dddba3832d428a32
adityaaryas <144906564+adityaaryas@users.noreply.github.com>
2023-10-16 16:52:27 +0700

    Update main.js Aditya

--- main.js
+++ main.js
@@ -8,7 +8,7 @@
 // console.log(Promnet)
 
 // console.log(Promnet)
-// var.Promnet = "Shadow Kill"
+// var Promnet = "Shadow Kill"
 // console.log(Promnet)
 
 // let Promnet = "Coding Isfa Lana"
@@ -31,34 +31,55 @@
 // let x = 6;
 // let y = 3;
 
-    // if(x < 10 && y > 1){
-    //     document.write("True");
-    // }else{
-    //     doucument.write("False");
-    // }
+//     if(x < 10 && y > 1){
+//         document.write("True");
+//     }else{
+//         doucument.write("False");
+//     }
     
 
-    // console.log(x < 10 && y > 1);
-    // console.log(x < 10 && y < 1);
-    // console.log (x == 5 || y==5);
-    // console.log(x==6 || y==5);
-    // console.log(! (x==y));
+//     console.log(x < 10 && y > 1);
+//     console.log(x < 10 && y < 1);
+//     console.log (x == 5 || y==5);
+//     console.log(x==6 || y==5);
+//     console.log(! (x==y));
 
-    // document.write(x < 10 && y > 1);
-    // document.write("<Br>");
-    // document.write(x < 10 && y < 1);
-    // document.write("<Br>");
-    // document.write(x == 5 || y==5);
-    // document.write("<Br>");
-    // document.write(x==6 || y==5);
-    // document.write("<Br>");
-    // document.write(! (x==y));
+//     document.write(x < 10 && y > 1);
+//     document.write("<Br>");
+//     document.write(x < 10 && y < 1);
+//     document.write("<Br>");
+//     document.write(x == 5 || y==5);
+//     document.write("<Br>");
+//     document.write(x==6 || y==5);
+//     document.write("<Br>");
+//     document.write(! (x==y));
 
-    // let p = document.querySelector("p")
-    // let button = document.querySelector("button")
-    // let input = document.querySelector("input")
-    // button.addEventListener('clik',function(){
-    //     let isi = input.value
-    //     console.log(isi)
-    //     p.innerHTML = isi
-    // })
+let p = document.querySelector("p");
+let button = document.querySelector("button");
+let input = document.querySelector("input");
+
+button.addEventListener('click', function () {
+    let isi = input.value;
+    console.log(isi);
+
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    let label = document.createElement("label");
+    label.appendChild(checkbox);
+    label.appendChild(document.createTextNode(isi));
+
+    p.appendChild(label);
+
+    p.appendChild(document.createElement("br"));
+
+    checkbox.addEventListener('change', function () {
+        if (checkbox.checked) {
+            label.style.textDecoration = 'line-through'; 
+        } else {
+            label.style.textDecoration = 'none'; 
+        }
+    });
+    input.value = "";
+});
+
